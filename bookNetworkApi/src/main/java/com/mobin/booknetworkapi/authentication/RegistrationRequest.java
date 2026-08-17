d95e18b3e9629b2a3327bf5133a4dfd61a7b983a
MObinXIV <mobinxiv@gmail.com>
2026-08-17 07:53:28 +0300

    add RegistrationRequest

---
+++ bookNetworkApi/src/main/java/com/mobin/booknetworkapi/authentication/RegistrationRequest.java
@@ -0,0 +1,24 @@
+package com.mobin.booknetworkapi.authentication;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class RegistrationRequest {
+    @NotBlank(message = "Firstname is mandatory")
+    private String firstName;
+    @NotBlank(message = "Lastname is mandatory")
+    private String lastName;
+    @Email(message = "Email is not formatted")
+    @NotBlank(message = "email is mandatory")
+    private String email;
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 8, message = "Password should be at least 8 character long")
+    private String password;
+}
