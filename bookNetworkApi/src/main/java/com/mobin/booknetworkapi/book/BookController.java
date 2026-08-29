e0ac6de47fe36ca490f6017d9f93893eb98672f2
MObinXIV <mobinxiv@gmail.com>
2026-08-29 19:00:11 +0300

    Implement the logic for getting all books except the current user ones.

--- bookNetworkApi/src/main/java/com/mobin/booknetworkapi/book/BookController.java
+++ bookNetworkApi/src/main/java/com/mobin/booknetworkapi/book/BookController.java
@@ -1,5 +1,6 @@
 package com.mobin.booknetworkapi.book;
 
+import com.mobin.booknetworkapi.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,5 +22,13 @@ public class BookController {
     @GetMapping("{book-id}")
     public ResponseEntity<BookResponse> findBookById(@PathVariable("book-id")Integer bookId){
         return ResponseEntity.ok(bookService.findById(bookId));
+    }
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>>findAllBooks(
+            @RequestParam(name = "page",defaultValue = "0",required = false)int page,
+            @RequestParam(name = "size",defaultValue = "10",required = false)int size,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(bookService.findAllBooks(page,size,connectedUser));
     }
 }
--- bookNetworkApi/src/main/java/com/mobin/booknetworkapi/book/BookService.java
+++ bookNetworkApi/src/main/java/com/mobin/booknetworkapi/book/BookService.java
@@ -1,10 +1,18 @@
 package com.mobin.booknetworkapi.book;
 
+import com.mobin.booknetworkapi.common.PageResponse;
 import com.mobin.booknetworkapi.user.User;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +30,23 @@ public class BookService {
         return bookRepository.findById(bookId)
                 .map(bookMapper::toBookResponse)
                 .orElseThrow(()-> new EntityNotFoundException("No Book Found with ID:: "+bookId));
+    }
+
+    public PageResponse<BookResponse> findAllBooks(int page, int size, Authentication connectedUser) {
+        User user = ((User) connectedUser.getPrincipal());
+        Pageable pageable= PageRequest.of(page,size, Sort.by("createdDate").descending());
+        Page<Book> books = bookRepository.findAllDisplayableBooks(pageable,user.getId());
+        List<BookResponse>bookResponse= books.stream()
+                .map(bookMapper::toBookResponse)
+                .toList();
+        return new PageResponse<>(
+                bookResponse,
+                books.getNumber(),
+                books.getSize(),
+                books.getTotalElements(),
+                books.getTotalPages(),
+                books.isFirst(),
+                books.isLast()
+        );
     }
 }
